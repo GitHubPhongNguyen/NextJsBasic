684fae093605310e09aec45952cf23e4ac5e9414
Him98 <nguyenvanphong5998@gmail.com>
2022-08-28 22:17:42 +0700

    [ADD] add optimize image

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ['images.unsplash.com'],
+  },
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
