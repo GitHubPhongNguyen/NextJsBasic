c6d6a19e9c89ae42e20a411ba6e6cec69202a3d1
Him98 <nguyenvanphong5998@gmail.com>
2022-08-31 00:36:13 +0700

    [UPDATE] update SEO

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Him98';
-export const siteTitle = 'Him98';
+export const siteTitle = 'Bloger Him98';
 
 export default function Layout({ children, home }) {
   return (
@@ -14,7 +14,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Đây là blog cá nhân của Him98."
         />
         <meta property="og:image" content={`/images/profile.jpg`} />
         <meta name="og:title" content={siteTitle} />
